7356122dcde57db42ecac64fd68dd3858668cb89
Jia Ye <planet0104@gmail.com>
2024-03-14 11:29:49 +0800

    test speed
    2000次，rust代码耗时8.9秒，4.45ms一帧，wgpu耗时5.9秒, 大概 2.97ms一帧

--- src/yuv2rgb.rs
+++ src/yuv2rgb.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::time::Instant;
 
 use anyhow::Ok;
 use anyhow::Result;
@@ -12,14 +13,27 @@ pub fn yuv2rgb() -> Result<()> {
     let src_image = image::load_from_memory(include_bytes!("../images/capture.jpg"))?.to_rgba8();
     let (width, height) = (src_image.width(), src_image.height());
     let src_yuv = include_bytes!("../images/capture.yuv");
+    
+    // let test_num = 2000;
+    // let t = Instant::now();
+    // let mut total_len = 0;
+    // for _ in 0..test_num{
+    //     let rgb_data = yuv_to_rgba_cpu(src_yuv, width as i32, height as i32);
+    //     total_len += rgb_data.len();
+    // }
+    // println!("CPU YUV to RGBA 转换耗时:{}ms total_len={total_len} 次数:{test_num}",t.elapsed().as_millis());
 
     //获取Y数据和UV数据
     let y_data = &src_yuv[..(width*height) as usize];
     let uv_data = &src_yuv[(width*height) as usize..];
 
-    println!("src_yuv:{}", src_yuv.len());
-    println!("y_data:{}", y_data.len());
-    println!("uv_data:{}", uv_data.len());
+    // println!("src_yuv:{}", src_yuv.len());
+    // println!("y_data:{}", y_data.len());
+    // println!("uv_data:{}", uv_data.len());
+
+    //------------------------------------------------------
+    // 初始化硬件设备
+    //------------------------------------------------------
 
     let instance = wgpu::Instance::default();
     let adapter = instance
@@ -41,6 +55,10 @@ pub fn yuv2rgb() -> Result<()> {
             None,
         )
         .block_on()?;
+
+    //------------------------------------------------------
+    // 创建 pipeline layout、compute pipeline、bind group layout 和 shader module
+    //------------------------------------------------------
 
     let compute_texture_yuv_bind_group_layout =
         device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -107,6 +125,10 @@ pub fn yuv2rgb() -> Result<()> {
         }),
         entry_point: "main",
     });
+
+    //------------------------------------------------------
+    // 创建纹理、纹理视图、采样器和缓冲区，并设置它们的相关描述符
+    //------------------------------------------------------
     
     let texture_size = wgpu::Extent3d {
         width,
@@ -182,40 +204,6 @@ pub fn yuv2rgb() -> Result<()> {
         ..Default::default()
     });
 
-    println!("write y_texture texture_size={:?}", texture_size);
-    queue.write_texture(
-        wgpu::ImageCopyTextureBase {
-            texture: &y_texture,
-            mip_level: 0,
-            origin: wgpu::Origin3d::ZERO,
-            aspect: wgpu::TextureAspect::All,
-        },
-        &y_data,
-        wgpu::ImageDataLayout {
-            offset: 0,
-            bytes_per_row: Some(width as u32),
-            rows_per_image: Some(height as u32),
-        },
-        texture_size,
-    );
-
-    println!("write u_texture u_size={:?}", u_size);
-    queue.write_texture(
-        wgpu::ImageCopyTextureBase {
-            texture: &u_texture,
-            mip_level: 0,
-            origin: wgpu::Origin3d::ZERO,
-            aspect: wgpu::TextureAspect::All,
-        },
-        &uv_data,
-        wgpu::ImageDataLayout {
-            offset: 0,
-            bytes_per_row: Some(width as u32),
-            rows_per_image: Some(height as u32),
-        },
-        u_size,
-    );
-
     let compute_yuv_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         layout: &compute_texture_yuv_bind_group_layout,
         entries: &[
@@ -239,63 +227,118 @@ pub fn yuv2rgb() -> Result<()> {
         label: Some("yuv_bind_group2"),
     });
 
-    let mut encoder = device.create_command_encoder(
-        &wgpu::CommandEncoderDescriptor { label: None },
-    );
+    // println!("write y_texture texture_size={:?}", texture_size);
 
-    {
-        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor::default() );
-        cpass.set_pipeline(&compute_pipeline_yuv);
-        cpass.set_bind_group(0, &compute_yuv_bind_group, &[]);
-        cpass.dispatch_workgroups(width / 8, height / 8, 1);
-    }
+    //------------------------------------------------------
+    // 创建encoder(命令编码器)
+    //------------------------------------------------------
 
-    let padded_bytes_per_row = padded_bytes_per_row(width);
-    let unpadded_bytes_per_row = width as usize * 4;
+    // let t = Instant::now();
+    // let mut total_len = 0;
+    // for _ in 0..test_num{
+        //------------------------------------------------------
+        // YUV数据写入纹理中
+        //------------------------------------------------------
 
-    let output_buffer_size =
-        padded_bytes_per_row as u64 * height as u64 * std::mem::size_of::<u8>() as u64;
-    let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: None,
-        size: output_buffer_size,
-        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
-        mapped_at_creation: false,
-    });
-
-    encoder.copy_texture_to_buffer(
-        wgpu::ImageCopyTexture {
-            aspect: wgpu::TextureAspect::All,
-            texture: &easu_texture,
-            mip_level: 0,
-            origin: wgpu::Origin3d::ZERO,
-        },
-        wgpu::ImageCopyBuffer {
-            buffer: &output_buffer,
-            layout: wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(padded_bytes_per_row as u32),
-                rows_per_image: Some(height),
+        queue.write_texture(
+            wgpu::ImageCopyTextureBase {
+                texture: &y_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
             },
-        },
-        texture_size,
-    );
+            &y_data,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(width as u32),
+                rows_per_image: Some(height as u32),
+            },
+            texture_size,
+        );
 
-    queue.submit(Some(encoder.finish()));
+        queue.write_texture(
+            wgpu::ImageCopyTextureBase {
+                texture: &u_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &uv_data,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(width as u32),
+                rows_per_image: Some(height as u32),
+            },
+            u_size,
+        );
+        
 
-    let buffer_slice = output_buffer.slice(..);
-    buffer_slice.map_async(wgpu::MapMode::Read, |_| {});
+        //------------------------------------------------------
+        // 开始新的计算 pass
+        //------------------------------------------------------
 
-    device.poll(wgpu::Maintain::Wait);
+        let mut encoder = device.create_command_encoder(
+            &wgpu::CommandEncoderDescriptor { label: None },
+        );
 
-    let padded_data = buffer_slice.get_mapped_range();
+        {
+            let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor::default() );
+            cpass.set_pipeline(&compute_pipeline_yuv);
+            cpass.set_bind_group(0, &compute_yuv_bind_group, &[]);
+            cpass.dispatch_workgroups(width / 8, height / 8, 1);
+        }
 
-    let mut pixels: Vec<u8> = vec![0; unpadded_bytes_per_row * height as usize];
-    for (padded, pixels) in padded_data
-        .chunks_exact(padded_bytes_per_row)
-        .zip(pixels.chunks_exact_mut(unpadded_bytes_per_row))
-    {
-        pixels.copy_from_slice(&padded[..unpadded_bytes_per_row]);
-    }
+        let padded_bytes_per_row = padded_bytes_per_row(width);
+        let unpadded_bytes_per_row = width as usize * 4;
+
+        let output_buffer_size =
+            padded_bytes_per_row as u64 * height as u64 * std::mem::size_of::<u8>() as u64;
+        let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: output_buffer_size,
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+            mapped_at_creation: false,
+        });
+
+        encoder.copy_texture_to_buffer(
+            wgpu::ImageCopyTexture {
+                aspect: wgpu::TextureAspect::All,
+                texture: &easu_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            wgpu::ImageCopyBuffer {
+                buffer: &output_buffer,
+                layout: wgpu::ImageDataLayout {
+                    offset: 0,
+                    bytes_per_row: Some(padded_bytes_per_row as u32),
+                    rows_per_image: Some(height),
+                },
+            },
+            texture_size,
+        );
+
+        queue.submit(Some(encoder.finish()));
+
+        let buffer_slice = output_buffer.slice(..);
+        buffer_slice.map_async(wgpu::MapMode::Read, |_| {});
+
+        device.poll(wgpu::Maintain::Wait);
+
+        let padded_data = buffer_slice.get_mapped_range();
+
+        let mut pixels: Vec<u8> = vec![0; unpadded_bytes_per_row * height as usize];
+        for (padded, pixels) in padded_data
+            .chunks_exact(padded_bytes_per_row)
+            .zip(pixels.chunks_exact_mut(unpadded_bytes_per_row))
+        {
+            pixels.copy_from_slice(&padded[..unpadded_bytes_per_row]);
+        }
+
+        // total_len += pixels.len();
+    // }
+    
+    // println!("GPU YUV to RGBA 转换耗时:{}ms total_len={total_len} 次数:{test_num}", t.elapsed().as_millis());
 
     if let Some(output_image) =
         image::ImageBuffer::<image::Rgba<u8>, _>::from_raw(width, height, &pixels[..])
@@ -303,4 +346,40 @@ pub fn yuv2rgb() -> Result<()> {
         output_image.save("./outputs/capture.png")?;
     }
     Ok(())
+}
+
+pub fn yuv_to_rgba_cpu(data:&[u8], width:i32, height:i32) -> Vec<u8>{
+    let frame_size = width * height;
+    let mut yp = 0;
+    let mut rgba_data = Vec::with_capacity(frame_size as usize*4);
+    for j in 0..height{
+        let (mut uvp, mut u, mut v) = ((frame_size + (j >> 1) * width) as usize, 0, 0);
+        for i in 0..width{
+            let mut y = (0xff & data[yp] as i32) - 16;  
+            if y < 0 { y = 0; }
+            if i & 1 == 0{
+                v = (0xff & data[uvp] as i32) - 128;
+                uvp += 1;
+                u = (0xff & data[uvp] as i32) - 128;  
+                uvp += 1;
+            }
+
+            let y1192 = 1192 * y;  
+            let mut r = y1192 + 1634 * v;
+            let mut g = y1192 - 833 * v - 400 * u;
+            let mut b = y1192 + 2066 * u;
+
+            if r < 0 { r = 0; } else if r > 262143 { r = 262143; };
+            if g < 0 { g = 0; } else if g > 262143 { g = 262143; }
+            if b < 0 { b = 0;} else if b > 262143 { b = 262143; }
+
+            let r = (r>>10) & 0xff;
+            let g = (g>>10) & 0xff;
+            let b = (b>>10) & 0xff;
+            rgba_data.extend_from_slice(&[r as u8, g as u8, b as u8, 255]);
+            yp += 1;
+        }
+    }
+
+    rgba_data
 }
